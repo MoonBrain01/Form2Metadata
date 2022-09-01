31d2715595e803a05be48f12cf636722d89706b8
MoonBrain1 <39500069+MoonBrain1@users.noreply.github.com>
2022-09-01 15:33:48 +0100

    Added style and namespaces to settings

--- metadata2form.py
+++ metadata2form.py
@@ -19,19 +19,18 @@ if not os.path.exists(full_path):
 # Read in the metadata file
 df_metadata = pd.read_excel(full_path, keep_default_na=False, dtype='str')
 
-settings_header = ('form_title', 'form_id', 'version')
-choices_header = ('list_name', 'label', 'name')
-survey_header = ('type', 'name', 'label', 'bind::oc:itemgroup', 'required')
-
+choices_cols = ('list_name', 'label', 'name')
+survey_cols = ('type', 'name', 'label', 'bind::oc:itemgroup', 'required')
+settings_cols = ('form_title', 'form_id', 'version', 'style', 'namespaces')
 # Initialise dataframes
-df_choices = pd.DataFrame(columns=choices_header)
-df_survey = pd.DataFrame(columns=survey_header)
+df_choices = pd.DataFrame(columns=choices_cols)
+df_survey = pd.DataFrame(columns=survey_cols)
 
-df_settings = pd.DataFrame(columns=settings_header)
+df_settings = pd.DataFrame(columns=settings_cols)
 form_title = df_metadata[df_metadata['Type'].isin(
     ['Form:', 'Form'])]['Description'].to_string(index=False)
 df_settings = df_settings.append(
-    {'form_title': form_title, 'form_id': '', 'version': '0'}, ignore_index=True)
+    dict(zip(settings_cols, (form_title, '', '0', 'theme-grid', 'oc="http://openclinica.org/xforms" , OpenClinica="http://openclinica.com/odm"'))), ignore_index=True)
 
 valid_types = ('note', 'integer', 'decimal', 'category', 'text')
 ques_count = 0
@@ -46,7 +45,7 @@ for row in df_metadata.itertuples():
     if is_select:
         if ques_type.isnumeric():
             df_choices = df_choices.append(
-                {'list_name': ques_code, 'label': ques_type, 'name': ques_label}, ignore_index=True)
+                dict(zip(choices_cols, (ques_code, ques_type, ques_label))), ignore_index=True)
             continue
         else:
             # If it is not a number, assume it is the end of the list options for the select question
@@ -60,13 +59,13 @@ for row in df_metadata.itertuples():
     ques_code = f"ques_{ques_count:04d}"
 
     if ques_type == 'category':
-        df_survey = df_survey.append({'type': f"select_one {ques_code}", 'name': ques_code,
-                                     'label': ques_label, 'bind::oc:itemgroup': 'main', 'required': 'yes'}, ignore_index=True)
+        df_survey = df_survey.append(
+            dict(zip(survey_cols, (f"select_one {ques_code}", ques_code, ques_label, 'main', 'yes'))), ignore_index=True)
         is_select = True
         continue
 
-    df_survey = df_survey.append({'type': ques_type, 'name': ques_code,
-                                 'label': ques_label, 'bind:oc:itemgroup': 'main'}, ignore_index=True)
+    df_survey = df_survey.append(dict(zip(
+        survey_cols, (ques_type, ques_code, ques_label, 'main', 'yes'))), ignore_index=True)
 
 # Convert the Metadata dataframe into an Excel object
 # https://openpyxl.readthedocs.io/en/stable/pandas.html#:~:text=Working%20with%20Pandas%20Dataframes%20%C2%B6%20The%20openpyxl.utils.dataframe.dataframe_to_rows%20%28%29,wb.active%20for%20r%20in%20dataframe_to_rows%28df%2C%20index%3DTrue%2C%20header%3DTrue%29%3A%20ws.append%28r%29
