901dd91860b7e8be9521e2599e82fc0715fbd6c4
MoonBrain1 <39500069+MoonBrain1@users.noreply.github.com>
2022-09-05 11:04:45 +0100

    added Group tags

--- metadata2form.py
+++ metadata2form.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import openpyxl as op
 from openpyxl.utils.dataframe import dataframe_to_rows
+import re
 
 import os
 import sys
@@ -22,20 +23,25 @@ if not os.path.exists(full_path):
 df_metadata = pd.read_excel(full_path, keep_default_na=False, dtype='str')
 
 choices_cols = ('list_name', 'label', 'name')
-survey_cols = ('type', 'name', 'label', 'bind::oc:itemgroup', 'required')
+survey_cols = ('type', 'name', 'label', 'bind::oc:itemgroup',
+               'required', 'appearance')
 settings_cols = ('form_title', 'form_id', 'version', 'style', 'namespaces')
 # Initialise dataframes
 df_choices = pd.DataFrame(columns=choices_cols)
 df_survey = pd.DataFrame(columns=survey_cols)
 
+# Populate Settings worksheet
 df_settings = pd.DataFrame(columns=settings_cols)
 form_title = df_metadata[df_metadata['Type'].isin(
     ['Form:', 'Form'])]['Description'].to_string(index=False)
 df_settings = df_settings.append(
     dict(zip(settings_cols, (form_title, '', '0', 'theme-grid', 'oc="http://openclinica.org/xforms" , OpenClinica="http://openclinica.com/odm"'))), ignore_index=True)
 
-valid_types = ('note', 'integer', 'decimal', 'category', 'text')
+valid_types = ('note', 'integer', 'decimal',
+               'category', 'text', 'date', 'group')
 ques_count = 0
+group_count = 0
+group_code_list = []
 is_select = False
 
 for row in df_metadata.itertuples():
@@ -54,7 +60,22 @@ for row in df_metadata.itertuples():
             is_select = False
 
     # Skip row if it is not a valid question type
-    if not (ques_type in valid_types):
+    if not (ques_type in valid_types or ques_type.startswith('group')):
+        continue
+
+    if re.search("^group\s*start$", ques_type):
+        group_count += 1
+        group_code = f"group_{group_count:03d}"
+        group_code_list.append([group_code, ques_label])
+
+        df_survey = df_survey.append(
+            dict(zip(survey_cols, ("group start", group_code, ques_label, 'main', '', 'field-list'))), ignore_index=True)
+        continue
+
+    if re.search("^group\s*end$", ques_type):
+        group_code, group_label = group_code_list.pop()
+        df_survey = df_survey.append(
+            dict(zip(survey_cols, ("group end", group_code, group_label, 'main', '', ''))), ignore_index=True)
         continue
 
     ques_count += 1
@@ -62,17 +83,17 @@ for row in df_metadata.itertuples():
 
     if ques_type == 'category':
         df_survey = df_survey.append(
-            dict(zip(survey_cols, (f"select_one {ques_code}", ques_code, ques_label, 'main', 'yes'))), ignore_index=True)
+            dict(zip(survey_cols, (f"select_one {ques_code}", ques_code, ques_label, 'main', 'yes', ''))), ignore_index=True)
         is_select = True
         continue
 
     if ques_type == 'note':
         # Notes cannot have a value in the Required and ItemGroup column
         df_survey = df_survey.append(dict(
-            zip(survey_cols, (ques_type, ques_code, ques_label, '', ''))), ignore_index=True)
+            zip(survey_cols, (ques_type, ques_code, ques_label, '', '', ''))), ignore_index=True)
     else:
         df_survey = df_survey.append(dict(zip(
-            survey_cols, (ques_type, ques_code, ques_label, 'main', 'yes'))), ignore_index=True)
+            survey_cols, (ques_type, ques_code, ques_label, 'main', 'yes', ''))), ignore_index=True)
 
 # Convert the Metadata dataframe into an Excel object
 # https://openpyxl.readthedocs.io/en/stable/pandas.html#:~:text=Working%20with%20Pandas%20Dataframes%20%C2%B6%20The%20openpyxl.utils.dataframe.dataframe_to_rows%20%28%29,wb.active%20for%20r%20in%20dataframe_to_rows%28df%2C%20index%3DTrue%2C%20header%3DTrue%29%3A%20ws.append%28r%29
