7a34dadf0ea9e250bde91fdf087614f3b09edbf0
MoonBrain01 <86025466+MoonBrain01@users.noreply.github.com>
2022-09-07 11:41:39 +0100

    Changed to use the "table-list" tag

--- metadata2form.py
+++ metadata2form.py
@@ -22,20 +22,18 @@ if not os.path.exists(full_path):
 # Read in the metadata file
 df_metadata = pd.read_excel(full_path, keep_default_na=False, dtype='str')
 
-choices_cols = ('list_name', 'label', 'name')
-survey_cols = ('type', 'name', 'label', 'bind::oc:itemgroup',
-               'required', 'appearance')
-settings_cols = ('form_title', 'form_id', 'version', 'style', 'namespaces')
 # Initialise dataframes
-df_choices = pd.DataFrame(columns=choices_cols)
-df_survey = pd.DataFrame(columns=survey_cols)
+df_choices = pd.DataFrame(columns=['list_name', 'label', 'name'])
+df_survey = pd.DataFrame(columns=['type', 'name', 'label', 'bind::oc:itemgroup',
+               'required', 'appearance'])
 
 # Populate Settings worksheet
-df_settings = pd.DataFrame(columns=settings_cols)
+
+df_settings = pd.DataFrame(columns=['form_title', 'form_id', 'version', 'style', 'namespaces'])
 form_title = df_metadata[df_metadata['Type'].isin(
     ['Form:', 'Form'])]['Description'].to_string(index=False)
-df_settings = df_settings.append(
-    dict(zip(settings_cols, (form_title, '', '0', 'theme-grid', 'oc="http://openclinica.org/xforms" , OpenClinica="http://openclinica.com/odm"'))), ignore_index=True)
+df_settings = df_settings.append({'form_title':form_title, 'form_id':'', 'version':'0', 'style':'theme-grid',
+    'namespaces':'oc="http://openclinica.org/xforms" , OpenClinica="http://openclinica.com/odm"'}, ignore_index=True)
 
 valid_types = ('note', 'integer', 'decimal',
                'category', 'text', 'date', 'group', 'table')
@@ -53,7 +51,7 @@ for row in df_metadata.itertuples():
     if is_select:
         if ques_type.isnumeric():
             df_choices = df_choices.append(
-                dict(zip(choices_cols, (ques_code, ques_label, ques_type))), ignore_index=True)
+                {'list_name':ques_code, 'label':ques_label, 'name':ques_type}, ignore_index=True)
             continue
         else:
             # If it is not a number, assume it is the end of the list options for the select question
@@ -63,70 +61,48 @@ for row in df_metadata.itertuples():
     if not (ques_type in valid_types or ques_type.startswith('group') or ques_type.startswith('table')):
         continue
 
-    #Group tags
-    if re.search("^group\s*start$", ques_type):
+    #Group/Table tags
+    if re.search("^group\s*start$", ques_type) or re.search("^table\s*start$", ques_type):
         group_count += 1
         group_code = f"group_{group_count:03d}"
         group_code_list.append([group_code, ques_label])
+        if re.search("^table\s*start$", ques_type):
+            is_table=True
+            group_appearance = 'table-list'
+        else:
+            group_appearance = 'field-list'
 
         df_survey = df_survey.append(
-            dict(zip(survey_cols, ("begin group", group_code, ques_label, 'main', '', 'field-list'))), ignore_index=True)
+            {'type':"begin group", 'name':group_code, 'label':ques_label, 'bind::oc:itemgroup':'', 'required':'', 'apperance':group_appearance}, ignore_index=True)
         continue
 
-    if re.search("^group\s*end$", ques_type):
+    if re.search("^group\s*end$", ques_type) or re.search("^table\s*end$", ques_type):
         group_code, group_label = group_code_list.pop()
-        df_survey = df_survey.append(
-            dict(zip(survey_cols, ("end group", group_code, group_label, 'main', '', ''))), ignore_index=True)
-        continue
-
-    #Table tags
-    if re.search("^table\s*start$", ques_type):
-        is_table = True
-        is_table_label = True
-        group_count += 1
-        group_code = f"group_{group_count:03d}"
-        group_code_list.append([group_code, ques_label])
+        if re.search("^table\s*start$", ques_type):
+            is_table=False
 
         df_survey = df_survey.append(
-            dict(zip(survey_cols, ("begin group", group_code, ques_label, 'main', '', 'field-list'))), ignore_index=True)
-        continue
-
-    if re.search("^table\s*end$", ques_type):
-        is_table = False
-        is_table_label = False
-        group_code, group_label = group_code_list.pop()
-        df_survey = df_survey.append(
-            dict(zip(survey_cols, ("end group", group_code, group_label, 'main', '', ''))), ignore_index=True)
+            {'type':"end group", 'name':group_code, 'label':group_label, 'bind::oc:itemgroup':'','required':'', 'appearance':''}, ignore_index=True)
         continue
 
     ques_count += 1
     ques_code = f"ques_{ques_count:04d}"
 
     if ques_type == 'category':
-        if is_table:
-            if is_table_label:
-                ques_label=''
-                ques_group=''
-                ques_required = ''
-                ques_appearance = 'label'
-                is_table_label=False
-            else:
-                ques_group='main'
-                ques_required = 'yes'
-                ques_appearance = 'list-nolabel'
 
         df_survey = df_survey.append(
-            dict(zip(survey_cols, (f"select_one {ques_code}", ques_code, ques_label, ques_group, ques_required, ques_appearance))), ignore_index=True)
+            {'type':f"select_one {ques_code}", 'name':ques_code, 'label':ques_label, 'bind::oc:itemgroup':'main', 'required':'yes', 'appearance':''}, ignore_index=True)
+
         is_select = True
         continue
 
     if ques_type == 'note':
         # Notes cannot have a value in the Required and ItemGroup column
-        df_survey = df_survey.append(dict(
-            zip(survey_cols, (ques_type, ques_code, ques_label, '', '', ''))), ignore_index=True)
+        df_survey = df_survey.append(
+            {'type':ques_type,'name':ques_code,'label':ques_label, 'bind::oc:itemgroup':'', 'required':'', 'appearance':''}, ignore_index=True)
     else:
-        df_survey = df_survey.append(dict(zip(
-            survey_cols, (ques_type, ques_code, ques_label, 'main', 'yes', ''))), ignore_index=True)
+        df_survey = df_survey.append(
+            {'type':ques_type, 'name':ques_code, 'label':ques_label, 'bind::oc:itemgroup':'main', 'required':'yes', 'appearance':''}, ignore_index=True)
 
 # Convert the Metadata dataframe into an Excel object
 # https://openpyxl.readthedocs.io/en/stable/pandas.html#:~:text=Working%20with%20Pandas%20Dataframes%20%C2%B6%20The%20openpyxl.utils.dataframe.dataframe_to_rows%20%28%29,wb.active%20for%20r%20in%20dataframe_to_rows%28df%2C%20index%3DTrue%2C%20header%3DTrue%29%3A%20ws.append%28r%29
